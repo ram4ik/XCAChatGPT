b8002fc7820a7f9bfe54125e54ee47c1b6663622
Ramill <ramill_ibragimov@icloud.com>
2023-02-05 09:42:06 +0200

    Initial Commit

---
+++ XCAChatGPT/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  XCAChatGPT
+//
+//  Created by Ramill Ibragimov on 05.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XCAChatGPT/XCAChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAChatGPTApp.swift
+//  XCAChatGPT
+//
+//  Created by Ramill Ibragimov on 05.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct XCAChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
